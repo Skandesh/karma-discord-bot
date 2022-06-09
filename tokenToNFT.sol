37a0e233b261b26b571ebc402814c3a1ae04e9aa
Skandesh <42321593+Skandesh@users.noreply.github.com>
2022-06-09 13:53:51 +0530

    Added comments

--- tokenToNFT.sol
+++ tokenToNFT.sol
@@ -6,28 +6,30 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, Ownable {
+contract TPG_NFT is ERC721, Ownable {
     using Counters for Counters.Counter;
 
-    IERC20 public myERC20Address;
-    uint256 public rate = 100 * 10**18;
+    IERC20 public TPGToken;
+    uint256 public rate = 100 * 10**18;  // have to hold 10 tokens for getting an NFT
 
     Counters.Counter private _tokenIdCounter;
 
 
 
-    constructor(address _myERC20Address) ERC721("MyToken", "MTK") {
-        myERC20Address = IERC20(_myERC20Address);
+    constructor(address _TPGToken) ERC721("TPG_NFT", "TPG") {
+        TPGToken = IERC20(_TPGToken);
     }
 
+//Mint the NFT using our tokens
     function safeMint() public  {
-        myERC20Address.transferFrom(msg.sender, address(this), rate);
+        TPGToken.transferFrom(msg.sender, address(this), rate);  
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
     }
 
+//Transfer back the tokens to the owner 
        function withdrawToken() public onlyOwner {
-        myERC20Address.transfer(msg.sender, myERC20Address.balanceOf(address(this)));
+        TPGToken.transfer(msg.sender, TPGToken.balanceOf(address(this)));
     }
 }
